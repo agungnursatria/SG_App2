3f6445499738392c9857bafa02081b0409c76738
Agung Nursatria <agungnursatria@gmail.com>
2018-09-05 17:42:09 +0700

    add direction

--- app/src/main/java/com/anb/sg_app2/MapsActivity.java
+++ app/src/main/java/com/anb/sg_app2/MapsActivity.java
@@ -3,6 +3,7 @@ package com.anb.sg_app2;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Address;
@@ -27,10 +28,16 @@ import com.akexorcist.googledirection.model.Direction;
 import com.akexorcist.googledirection.model.Leg;
 import com.akexorcist.googledirection.util.DirectionConverter;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
@@ -42,7 +49,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -68,22 +77,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             new LatLng(-40, -168), new LatLng(71, 136));
     final static float default_zoom = 15f;
 
-    Boolean permission;
-//    FusedLocationProviderClient mFusedLocationClient;
-//    LatLng origin, destination;
+    Boolean permission = false;
+    Boolean gpsUsable = false;
+    FusedLocationProviderClient mFusedLocationClient;
+    LatLng origin, destination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        requestPermission();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         ButterKnife.bind(this);
 
-//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (permission)
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -108,20 +120,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        requestPermission();
-        if (permission){
+        if (permission) {
             mMap.setMyLocationEnabled(true);
             init();
         }
     }
 
-    public void requestPermission(){
+    public void requestPermission() {
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                     && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(MapsActivity.this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
-                        1);
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
             }
         } else {
             permission = true;
@@ -133,13 +143,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .Builder(this)
                 .addApi(Places.GEO_DATA_API)
                 .addApi(Places.PLACE_DETECTION_API)
+                .addApi(LocationServices.API)
                 .enableAutoManage(this, this)
                 .build();
         mGoogleApiClient.connect();
 
         mPlaceAutoCompleteAdapter = new PlaceAutoCompleteAdapter(this, mGoogleApiClient,
                 LAT_LNG_BOUNDS, null);
-
         mSearchText.setAdapter(mPlaceAutoCompleteAdapter);
 
         mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -156,14 +166,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+        // Turn on GPS with high accuracy
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(30 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> task =
+                LocationServices.getSettingsClient(this).checkLocationSettings(builder.build());
+
+        task.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    LocationSettingsStates state = response.getLocationSettingsStates();
+                    if (state.isGpsPresent() && state.isGpsUsable()) {
+                        gpsUsable = true;
+                        if (permission) {
+                            mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                                @Override
+                                public void onSuccess(Location location) {
+                                    if (location != null) {
+                                        origin = new LatLng(location.getLatitude(), location.getLongitude());
+                                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(origin, 18f));
+                                    }
+                                }
+                            });
+                        }
+                    }
+                } catch (ApiException exception) {
+                    switch (exception.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            // Location settings are not satisfied. But could be fixed by showing the
+                            // user a dialog.
+                            try {
+                                // Cast to a resolvable exception.
+                                ResolvableApiException resolvable = (ResolvableApiException) exception;
+                                // Show the dialog by calling startResolutionForResult(),
+                                // and check the result in onActivityResult().
+                                resolvable.startResolutionForResult(MapsActivity.this, 1000);
+                            } catch (IntentSender.SendIntentException | ClassCastException e) {
+                                // Ignore the error.
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            break;
+                    }
+                }
+            }
+        });
+
         hideSoftKeyboard();
     }
 
     @SuppressLint("MissingPermission")
     private void geoLocate() {
-
         String searchString = mSearchText.getText().toString();
-
         Geocoder geocoder = new Geocoder(MapsActivity.this);
         List<Address> list = new ArrayList<>();
         try {
@@ -176,52 +238,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), default_zoom,
                     address.getAddressLine(0));
 
-//            destination = new LatLng(address.getLatitude(), address.getLongitude());
-//            if (permission){
-//                mFusedLocationClient.getLastLocation()
-//                        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-//                            @Override
-//                            public void onSuccess(Location location) {
-//                                if (location != null) {
-//                                    origin = new LatLng(location.getLatitude(), location.getLongitude());
-//                                    getDirection(origin, destination);
-//                                }
-//                            }
-//                        });
-//            }
+            destination = new LatLng(address.getLatitude(), address.getLongitude());
+            if (permission) {
+                mFusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                if (location != null) {
+                                    origin = new LatLng(location.getLatitude(), location.getLongitude());
+                                    getDirection(origin, destination);
+                                }
+                            }
+                        });
+            }
         }
     }
 
-    // Butuh API Google Platform (isi visa dlu)
-//    void getDirection(LatLng origin, LatLng destination){
-//        GoogleDirection.withServerKey(getString(R.string.google_maps_key))
-//                .from(origin)
-//                .to(destination)
-//                .transportMode(transportMode)
-//                .execute(new DirectionCallback() {
-//                    @Override
-//                    public void onDirectionSuccess(Direction direction, String rawBody) {
-//                        if(direction.isOK()) {
-//                            Leg leg = direction.getRouteList().get(0).getLegList().get(0);
-//                            ArrayList<LatLng> pointList = leg.getDirectionPoint();
-//                            PolylineOptions polylineOptions = DirectionConverter.createPolyline(MapsActivity.this, pointList, 5, Color.RED);
-//                            mMap.addPolyline(polylineOptions);
-//                        } else {
-//                            Toast.makeText(MapsActivity.this,direction.getErrorMessage(), Toast.LENGTH_SHORT).show();
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onDirectionFailure(Throwable t) {
-//                        Toast.makeText(MapsActivity.this,"Failure Directing", Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//    }
+    void getDirection(LatLng origin, LatLng destination) {
+        GoogleDirection.withServerKey(getString(R.string.google_maps_key))
+                .from(origin)
+                .to(destination)
+                .transportMode(transportMode)
+                .execute(new DirectionCallback() {
+                    @Override
+                    public void onDirectionSuccess(Direction direction, String rawBody) {
+                        if (direction.isOK()) {
+                            List<Leg> legs = direction.getRouteList().get(0).getLegList();
+                            for (int i = 0; i<legs.size(); i++){
+                                int color = (i==0) ? Color.RED : Color.GRAY;
+                                ArrayList<LatLng> pointList = legs.get(i).getDirectionPoint();
+                                PolylineOptions polylineOptions = DirectionConverter.createPolyline(MapsActivity.this, pointList, 5, color);
+                                mMap.addPolyline(polylineOptions);
+                            }
+                        } else {
+                            Toast.makeText(MapsActivity.this, direction.getErrorMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
 
-    private void moveCamera(LatLng latLng, float zoom, String title){
+                    @Override
+                    public void onDirectionFailure(Throwable t) {
+                        Toast.makeText(MapsActivity.this, "Failure Directing", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void moveCamera(LatLng latLng, float zoom, String title) {
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
 
-        if(!title.equals("My Location")){
+        if (!title.equals("My Location")) {
             MarkerOptions options = new MarkerOptions()
                     .position(latLng)
                     .title(title);
@@ -231,14 +295,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         hideSoftKeyboard();
     }
 
-    private void hideSoftKeyboard(){
+    private void hideSoftKeyboard() {
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
     @SuppressLint("MissingPermission")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
+        switch (requestCode) {
             case 1:
                 permission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
                 break;
